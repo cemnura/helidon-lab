c079cc89ff3103e744c1d78c32790a429915d046
cem.nura <cem.nura@gmail.com>
2019-04-30 11:02:23 +0300

    Changed POST append method return repsonse

--- demo/quote/src/main/java/com/cemnura/service/QuoteHandler.java
+++ demo/quote/src/main/java/com/cemnura/service/QuoteHandler.java
@@ -57,7 +57,7 @@ public class QuoteHandler implements Service {
             span.finish();
         }
 
-        res.send("Registered");
+        res.send("Success");
         decProgressGauge();
     }
 
